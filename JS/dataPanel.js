a9b6e5670944f1ca2022863a8cec0dcdebf7d1bd
Alexander Stadelmann <hi@alexander-stadelmann.de>
2023-07-10 18:22:20 +0200

    refactor: replaced .at(-1)

--- JS/dataPanel.js
+++ JS/dataPanel.js
@@ -3,6 +3,6 @@ function updateDataPanel() {
   document.querySelectorAll('.data').forEach((elem) => {
     // using a const from another file without any contract. This depends on order of the files
     // -1 magic number
-    elem.value = dynamicServerState.at(-1)[elem.id]
+    elem.value = getLastElem(dynamicServerState)[elem.id]
   })
 }
